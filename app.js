60172779b9b955b447f4dc69c113978773e4ee19
Cristian Prundeanu <cristian.prundeanu@arm.com>
2017-05-05 07:38:53 -0500

    Add accelerator, sound level, temperature, and luminosity
    Signed-off-by: Cristian Prundeanu <cristian.prundeanu@arm.com>

--- app.js
+++ app.js
@@ -12,11 +12,18 @@ var MbedConnectorApi = require('mbed-connector-api');
 // CONFIG (change these)
 var accessKey = process.env.ACCESS_KEY || "ChangeMe";
 var port = process.env.PORT || 8080;
+var bindhost = process.env.HOST || localhost;
 
 // Paths to resources on the endpoints
 var blinkResourceURI = '/3201/0/5850';
 var blinkPatternResourceURI = '/3201/0/5853';
 var buttonResourceURI = '/3200/0/5501';
+var accelXResourceURI = '/3313/0/5702';
+var accelYResourceURI = '/3313/0/5703';
+var accelZResourceURI = '/3313/0/5704';
+var soundLevelResourceURI = '/3324/0/5600';
+var temperatureResourceURI = '/3303/0/5600';
+var lightLevelResourceURI = '/3301/0/5600';
 
 // Instantiate an mbed Device Connector object
 var mbedConnectorApi = new MbedConnectorApi({
@@ -81,6 +88,7 @@ io.on('connection', function (socket) {
 
   socket.on('subscribe-to-presses', function (data) {
     // Subscribe to all changes of resource /3200/0/5501 (button presses)
+    console.log('Subscribing to button press updates');
     mbedConnectorApi.putResourceSubscription(data.endpointName, buttonResourceURI, function(error) {
       if (error) throw error;
       socket.emit('subscribed-to-presses', {
@@ -89,11 +97,104 @@ io.on('connection', function (socket) {
     });
   });
 
+  socket.on('subscribe-to-accel', function (data) {
+    // Subscribe to accel data
+    console.log('Subscribing to accelerator updates');
+    mbedConnectorApi.putResourceSubscription(data.endpointName, accelXResourceURI, function(error) {
+      if (error) throw error;
+      socket.emit('subscribed-to-accel-x', {
+        endpointName: data.endpointName
+      });
+    });
+    mbedConnectorApi.putResourceSubscription(data.endpointName, accelYResourceURI, function(error) {
+      if (error) throw error;
+      socket.emit('subscribed-to-accel-y', {
+        endpointName: data.endpointName
+      });
+    });
+    mbedConnectorApi.putResourceSubscription(data.endpointName, accelZResourceURI, function(error) {
+      if (error) throw error;
+      socket.emit('subscribed-to-accel-z', {
+        endpointName: data.endpointName
+      });
+    });
+  });
+
+  socket.on('subscribe-to-analog', function (data) {
+    // Subscribe to all changes of analog input resources
+    console.log('Subscribing to analog input updates');
+    mbedConnectorApi.putResourceSubscription(data.endpointName, soundLevelResourceURI, function(error) {
+      if (error) throw error;
+      socket.emit('subscribed-to-sound-level', {
+        endpointName: data.endpointName
+      });
+    });
+    mbedConnectorApi.putResourceSubscription(data.endpointName, temperatureResourceURI, function(error) {
+      if (error) throw error;
+      socket.emit('subscribed-to-temperature', {
+        endpointName: data.endpointName
+      });
+    });
+    mbedConnectorApi.putResourceSubscription(data.endpointName, lightLevelResourceURI, function(error) {
+      if (error) throw error;
+      socket.emit('subscribed-to-light-level', {
+        endpointName: data.endpointName
+      });
+    });
+  });
+
   socket.on('unsubscribe-to-presses', function(data) {
     // Unsubscribe from the resource /3200/0/5501 (button presses)
+    console.log('Unsubscribing from button press updates');
     mbedConnectorApi.deleteResourceSubscription(data.endpointName, buttonResourceURI, function(error) {
       if (error) throw error;
       socket.emit('unsubscribed-to-presses', {
+        endpointName: data.endpointName
+      });
+    });
+  });
+
+  socket.on('unsubscribe-to-accel', function(data) {
+    // Unsubscribe from accel data
+    console.log('Unsubscribing from accelerator updates');
+    mbedConnectorApi.deleteResourceSubscription(data.endpointName, accelXResourceURI, function(error) {
+      if (error) throw error;
+      socket.emit('unsubscribed-to-accel-x', {
+        endpointName: data.endpointName
+      });
+    });
+    mbedConnectorApi.deleteResourceSubscription(data.endpointName, accelYResourceURI, function(error) {
+      if (error) throw error;
+      socket.emit('unsubscribed-to-accel-y', {
+        endpointName: data.endpointName
+      });
+    });
+    mbedConnectorApi.deleteResourceSubscription(data.endpointName, accelZResourceURI, function(error) {
+      if (error) throw error;
+      socket.emit('unsubscribed-to-accel-z', {
+        endpointName: data.endpointName
+      });
+    });
+  });
+
+  socket.on('unsubscribe-to-analog', function(data) {
+    // Unsubscribe from the analog input resources
+    console.log('Unsubscribing from analog input updates');
+    mbedConnectorApi.deleteResourceSubscription(data.endpointName, soundLevelResourceURI, function(error) {
+      if (error) throw error;
+      socket.emit('unsubscribed-to-sound-level', {
+        endpointName: data.endpointName
+      });
+    });
+    mbedConnectorApi.deleteResourceSubscription(data.endpointName, temperatureResourceURI, function(error) {
+      if (error) throw error;
+      socket.emit('unsubscribed-to-temperature', {
+        endpointName: data.endpointName
+      });
+    });
+    mbedConnectorApi.deleteResourceSubscription(data.endpointName, lightLevelResourceURI, function(error) {
+      if (error) throw error;
+      socket.emit('unsubscribed-to-light-level', {
         endpointName: data.endpointName
       });
     });
@@ -104,6 +205,56 @@ io.on('connection', function (socket) {
     mbedConnectorApi.getResourceValue(data.endpointName, buttonResourceURI, function(error, value) {
       if (error) throw error;
       socket.emit('presses', {
+        endpointName: data.endpointName,
+        value: value
+      });
+    });
+  });
+
+  socket.on('get-accel', function(data) {
+    // Read data from GET resource /3313/0/5702 (accel)
+    mbedConnectorApi.getResourceValue(data.endpointName, accelXResourceURI, function(error, value) {
+      if (error) throw error;
+      socket.emit('accel-x', {
+        endpointName: data.endpointName,
+        value: value
+      });
+    });
+    mbedConnectorApi.getResourceValue(data.endpointName, accelYResourceURI, function(error, value) {
+      if (error) throw error;
+      socket.emit('accel-y', {
+        endpointName: data.endpointName,
+        value: value
+      });
+    });
+    mbedConnectorApi.getResourceValue(data.endpointName, accelZResourceURI, function(error, value) {
+      if (error) throw error;
+      socket.emit('accel-z', {
+        endpointName: data.endpointName,
+        value: value
+      });
+    });
+  });
+
+  socket.on('get-analog', function(data) {
+    // Read data from analog in GET resources
+    mbedConnectorApi.getResourceValue(data.endpointName, soundLevelResourceURI, function(error, value) {
+      if (error) throw error;
+      socket.emit('sound-level', {
+        endpointName: data.endpointName,
+        value: value
+      });
+    });
+    mbedConnectorApi.getResourceValue(data.endpointName, temperatureResourceURI, function(error, value) {
+      if (error) throw error;
+      socket.emit('temperature', {
+        endpointName: data.endpointName,
+        value: value
+      });
+    });
+    mbedConnectorApi.getResourceValue(data.endpointName, lightLevelResourceURI, function(error, value) {
+      if (error) throw error;
+      socket.emit('light-level', {
         endpointName: data.endpointName,
         value: value
       });
@@ -136,9 +287,58 @@ io.on('connection', function (socket) {
 // When notifications are received through the notification channel, pass the
 // button presses data to all connected browser windows
 mbedConnectorApi.on('notification', function(notification) {
+  //console.log('Notification for %s : %s', notification.path, notification.payload);
   if (notification.path === buttonResourceURI) {
     sockets.forEach(function(socket) {
       socket.emit('presses', {
+        endpointName: notification.ep,
+        value: notification.payload
+      });
+    });
+  } else
+  if (notification.path === accelXResourceURI) {
+    sockets.forEach(function(socket) {
+      socket.emit('accel-x', {
+        endpointName: notification.ep,
+        value: notification.payload
+      });
+    });
+  } else
+  if (notification.path === accelYResourceURI) {
+    sockets.forEach(function(socket) {
+      socket.emit('accel-y', {
+        endpointName: notification.ep,
+        value: notification.payload
+      });
+    });
+  } else
+  if (notification.path === accelZResourceURI) {
+    sockets.forEach(function(socket) {
+      socket.emit('accel-z', {
+        endpointName: notification.ep,
+        value: notification.payload
+      });
+    });
+  } else
+  if (notification.path === soundLevelResourceURI) {
+    sockets.forEach(function(socket) {
+      socket.emit('sound-level', {
+        endpointName: notification.ep,
+        value: notification.payload
+      });
+    });
+  } else
+  if (notification.path === temperatureResourceURI) {
+    sockets.forEach(function(socket) {
+      socket.emit('temperature', {
+        endpointName: notification.ep,
+        value: notification.payload
+      });
+    });
+  } else
+  if (notification.path === lightLevelResourceURI) {
+    sockets.forEach(function(socket) {
+      socket.emit('light-level', {
         endpointName: notification.ep,
         value: notification.payload
       });
@@ -147,10 +347,10 @@ mbedConnectorApi.on('notification', function(notification) {
 });
 
 // Start the app
-server.listen(port, function() {
+server.listen({host: bindhost, port: port}, function() {
   // Set up the notification channel (pull notifications)
   mbedConnectorApi.startLongPolling(function(error) {
     if (error) throw error;
-    console.log('mbed Device Connector Quickstart listening at http://localhost:%s', port);
+    console.log('ExD TSDB Connector Test Webapp listening at %s:%s', bindhost, port);
   })
 });
